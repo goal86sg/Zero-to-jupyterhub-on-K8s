6b7131fa98d3e06a10716e5f1a56a6c74f185d29
des <goal86sg@hotmail.com>
2021-01-05 02:56:58 +0000

    all changes

--- on-prem/pull_images.sh
+++ on-prem/pull_images.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat $HOME/Zero-to-jupyterhub-on-K8s/on-prem/images.txt | xargs -I {} -- sh -c 'docker pull {}'
+cat $HOME/Zero-to-jupyterhub-on-K8s/on-prem/imagesnew.txt | xargs -I {} -- sh -c 'docker pull {}'
--- on-prem/push_images.sh
+++ on-prem/push_images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Enter your private registry URL"
 read PRIVATE_REGISTRY
-cat $HOME/Zero-to-jupyterhub-on-K8s/on-prem/images.txt | xargs -I {} -- sh -c 'docker tag {} '"$PRIVATE_REGISTRY"'/{} && docker push '"$PRIVATE_REGISTRY"'/{}'
+cat $HOME/Zero-to-jupyterhub-on-K8s/on-prem/imagesnew.txt | xargs -I {} -- sh -c 'docker tag {} '"$PRIVATE_REGISTRY"'/{} && docker push '"$PRIVATE_REGISTRY"'/{}'
